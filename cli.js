55b377398fea471586d15f7d367a22b35f5978f5
atzina <atzina@gmail.com>
2023-01-15 19:28:55 -0600

    primeros pasos de la función mdLinks

---
+++ cli.js
@@ -0,0 +1,6 @@
+// donde va a leer los argumentos de línea de comando y pasarlo a mdlinks
+const { mdLinks } = require('./index.js');
+mdLinks('/noexiste').then(()=>{})
+.catch((error)=>{
+    console.log(error)
+});
