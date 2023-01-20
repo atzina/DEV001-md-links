f858c822e1ed9a9c3e961154fd6ab8ac3217b031
atzina <atzina@gmail.com>
2023-01-19 20:23:03 -0600

    Se añade la modulación, y los test de md-Links para cuando la ruta existe y no existe

--- cli.js
+++ cli.js
@@ -1,6 +1,15 @@
 // donde va a leer los argumentos de línea de comando y pasarlo a mdlinks
 const { mdLinks } = require('./index.js');
-mdLinks('/noexiste').then(()=>{})
+
+mdLinks('/noexiste').then(()=>{
+
+})
 .catch((error)=>{
     console.log(error)
 });
+
+
+// mdLinks('/C:\Users\AT\Desktop\README').then(()=>{})
+// .then((sigue)=>{
+//     console.log(sigue)
+// });
