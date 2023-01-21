45e7cbbc7518b3e35d595d22cc03693c8cc1822a
atzina <atzina@gmail.com>
2023-01-20 19:17:26 -0600

    la función dentro de md-links que cambia de ruta relativa a absoluta y su test

--- functions.js
+++ functions.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
 const pathExists = (route) => fs.existsSync(route);
+const pathIsAbsolute = (AbsoluteRoute) => path.isAbsolute(AbsoluteRoute);
+// ternario = Si la ruta es absoluta es true, devolver la ruta,
+// de lo contrario aplicar path.resolve para convertirla.
+const turnPathAbsolute = (route) => (pathIsAbsolute(route) ? route : path.resolve(route));
 
 module.exports = {
   pathExists,
+  pathIsAbsolute,
+  turnPathAbsolute,
 };
--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const { pathExists } = require('./functions');
+const { pathIsAbsolute, turnPathAbsolute } = require('./functions');
 
 const mdLinks = (path, options) => new Promise((resolve, reject) => {
   // Existe la ruta?
   if (!pathExists(path)) {
     reject(new Error('la ruta no existe'));
     // checar si es una ruta absoluta
-    // función que diga si es archivo o directorio.
-    // prueba la extención y compara con md.
     // escribir una funcion q devuelva todos los mds que viva en esta ruta
-  } else {
+  } if (!pathIsAbsolute(path)) {
+    resolve(turnPathAbsolute(path));
     // Sino existe se rechaza la promesa
-    resolve('si existe la ruta'); // que esté en el if y no en elese?
+    // resolve('si existe la ruta')
+    // checa si la ruta es absoluta devuelve true si es absoluta y false si es relativa
+    // resolve(pathIsAbsolute(path));
+    // si devuelve false convertir en absoluta;
   }
 });
 console.log(pathExists('C:/Users/AT/Documents/DEV001-md-links/Prueba/ejemplo.md'));
 console.log(pathExists('C:/noexsiste.md'));
+console.log(pathIsAbsolute('./functions)'));
+console.log(turnPathAbsolute('./functions)'));
 
 module.exports = {
   mdLinks,
